6d2df0269d8c391e0759e45d3c7362e3d97d5f9f
Sean Upton <sdupton@gmail.com>
2019-04-23 10:13:35 -0600

    engine_cart TestAppGenerator: add steps for installing Hyrax, DB migrations, configuring browse_everything in .internal_test_app

--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -1,14 +1,25 @@
-        require 'rails/generators'
+require 'rails/generators'
 
-        class TestAppGenerator < Rails::Generators::Base
-          source_root "./spec/test_app_templates"
+class TestAppGenerator < Rails::Generators::Base
+  source_root "./spec/test_app_templates"
 
-          # if you need to generate any additional configuration
-          # into the test app, this generator will be run immediately
-          # after setting up the application
+  # if you need to generate any additional configuration
+  # into the test app, this generator will be run immediately
+  # after setting up the application
 
-          def install_engine
-            generate 'work_files:install'
-          end
-        end
+  def install_hyrax
+    generate 'hyrax:install', '-f'
+  end
 
+  def install_engine
+    generate 'work_files:install'
+  end
+
+  def db_migrations
+    rake 'db:migrate'
+  end
+
+  def configure_browse_everything
+    generate 'browse_everything:config'
+  end
+end
